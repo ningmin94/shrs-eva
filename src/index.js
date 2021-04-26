00f15ec61feb1c8ccd7529fad15f1a018763278b
ningmin <603565766@qq.com>
2021-04-26 21:52:20 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
-console.log('init')
+import { Game, resource, GameObject } from '@eva/eva.js'
+import { RendererSystem } from '@eva/plugin-renderer'
+import { Img, ImgSystem } from '@eva/plugin-renderer-img' // 引入渲染图片所需要的组件和系统
+import resources from './resources.js'
+
+// 创建渲染系统
+const renderSystem = new RendererSystem({
+    canvas: document.querySelector('#canvas'), // 可选，自动生成 canvas 挂在 game.canvas 上
+    width: 750,
+    height: 1000,
+    transparent: false,
+    resolution: window.devicePixelRatio / 2, // 可选, 如果是2倍图设计 可以除以2
+    preventScroll: false, // 阻止页面滚动
+    renderType: 0 // 0:自动判断，1: WebGL，2:Canvas
+})
+
+resource.addResource(resources);
+
+// 创建游戏对象
+const game = new Game({
+    frameRate: 60, // 可选，游戏帧率，默认60
+    autoStart: true, // 可选，自动开始
+    systems: [renderSystem]
+})
+
+game.addSystem(new ImgSystem()) // 给游戏添加渲染图片的能力
+
+const gameObject = new GameObject('gameObj1', {
+    size: {
+        width: 658,
+        height: 1152
+    }
+})
+
+gameObject.addComponent(
+    new Img({
+        resource: 'image1'
+    })
+)
+
+game.scene.addChild(gameObject) // 把游戏对象放入场景，这样画布上就可以显示这张图片了
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,51 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-    mode: 'development',
-    entry: {
-        index: './src/index.js',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: '开始',
-        }),
-    ],
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist',
-    },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true
-    },
+module.exports = (env) => {
+    return {
+        mode: 'development',
+        entry: {
+            index: './src/index.js',
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: 'src/index.html'
+            }),
+        ],
+        devtool: 'inline-source-map',
+        devServer: {
+            contentBase: './dist',
+        },
+        output: {
+            filename: '[name].[contenthash].bundle.js',
+            path: path.resolve(__dirname, 'dist'),
+            clean: true
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.html$/,
+                    loader: 'html-loader'
+                }
+            ]
+        },
+        resolve: {
+            // https://github.com/babel/babel/issues/8462
+            // https://blog.csdn.net/qq_39807732/article/details/110089893
+            // 如果确认需要node polyfill，设置resolve.fallback安装对应的依赖
+            fallback: {
+            //   crypto: require.resolve('crypto-browserify'),
+              path: require.resolve('path-browserify'),
+            //   url: require.resolve('url'),
+            //   buffer: require.resolve('buffer/'),
+            //   util: require.resolve('util/'),
+            //   stream: require.resolve('stream-browserify/'),
+            //   vm: require.resolve('vm-browserify')
+            },
+            // 如果确认不需要node polyfill，设置resolve.alias设置为false
+            // alias: {
+            //   crypto: false
+            // }
+          }
+    }
 };
